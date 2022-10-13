1c93419a668cd6e353c3f97864b62d1af7b38c50
Anna <ann.sxd@example.com>
2022-10-13 13:49:56 +0300

    1 задача

---
+++ 01_Max_Min/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите 1 число: ");
+int numberA = int.Parse(Console.ReadLine()); 
+Console.Write("Введите 2 число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB) 
+{
+    Console.WriteLine($"max = {numberA}, min = {numberB}");
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine($"max = {numberB}, min = {numberA}");
+}
+else if (numberA == numberB)
+{
+    Console.WriteLine("Введенные числа равны");
+}
