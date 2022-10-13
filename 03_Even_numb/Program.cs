b8a8355cf895054a41d9ae62207bcc2b5d4effe1
Anna <ann.sxd@example.com>
2022-10-13 16:18:39 +0300

    3 задача

---
+++ 03_Even_numb/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.Clear();
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()); 
+
+if(numberA%2 == 0)
+{
+    Console.WriteLine($"Число {numberA} является ЧЁТНЫМ");
+}
+else
+{
+    Console.WriteLine($"Число {numberA} является НЕЧЁТНЫМ");
+}
